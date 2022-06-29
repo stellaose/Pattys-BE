e1de9eb6020f57f7a2cf467bb6acef6f5ad87f39
Stella Oseyomon <78904383+stellaose@users.noreply.github.com>
2022-06-29 22:44:13 +0100

    order controller created

---
+++ server/Routes/OrderRoutes.js
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import OrderController from '../Controller/OrderController.js';
+import { Auth, AllowedRoles }  from '../Middleware/Auth.js';
+
+
+const router = Router();
+
+router.post('/new-order', Auth, OrderController.newOrder);
+router.get('/one-order/:id', Auth, OrderController.getOneOrder);
+router.get('/orders/me', Auth, OrderController.getMyOrders);
+
+// * admin routes
+router.get('/admin/all-order', Auth, AllowedRoles('admin'), OrderController.getAllOrders);
+router.put('/admin/update-order/:id', Auth, AllowedRoles("admin"), OrderController.updateOrderStatus);
+router.delete('/admin/delete-order/:id', Auth, AllowedRoles("admin"), OrderController.deleteOrder)
+
+
+export default router;
--- server/Routes/index.js
+++ server/Routes/index.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 
 import ProductRoute from './ProductRoutes.js'
 import UserRoute from './UserRoutes.js'
+import OrderRoute from './OrderRoutes.js'
 
 const router = Router()
 
 router.use('/user', UserRoute);
 router.use('/product', ProductRoute);
+router.use('/order', OrderRoute)
 
 
 export default router
