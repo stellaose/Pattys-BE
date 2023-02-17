1f1e44d500696fa1b68be33cbc004cf3e305a2ad
Stella Oseyomon <78904383+stellaose@users.noreply.github.com>
2023-02-17 03:33:54 +0100

    upload fix

--- server/Controller/UserController.js
+++ server/Controller/UserController.js
@@ -106,239 +106,238 @@ const UserController = {
       }
     },
     
-    googleLogin: async (req, res, next) => {
+  googleLogin: async (req, res, next) => {
       
-    },
+  },
 
-    forgetPassword: async (req, res, next) => {
-        const { email } = req.body;
+  forgetPassword: async (req, res, next) => {
+    const { email } = req.body;
 
-        try{
-            const savedUser = await User.findOne({email});
+    try{
+      const savedUser = await User.findOne({email});
 
-            if(!savedUser){
-                return next 
-                    (new ErrorResponse('This email does not exist', 401))
-            }
-
-          // * Generate token
-          const resetToken = crypto.randomBytes(20).toString('hex');
-
-          // + Hash and set to resetPasswordToken
-          savedUser.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
-  
-          // - Set token expire time
-          savedUser.resetPasswordExpire = Date.now() + 30 * 60 * 1000
-  
-          await savedUser.save({validateBeforeSave: false});
-  
-          // ; create password reset url
-          const url = `${req.protocol}://${req.get(
-            "host"
-          )}/v1/user/reset-password/${resetToken}`;
-  
-          try {
-              await sendEmail( 
-                email, 
-                url, 
-                "Reset your password"
-                )
-  
-              res.json({
-                  status: 200,
-                  success: true,
-                  message: `Email sent to ${savedUser.email}`
-              })
-          } catch (error) {
-              savedUser.resetPasswordToken = undefined;
-              savedUser.resetPasswordExpire = undefined;
-  
-              await savedUser.save({validateBeforeSave: false})
-              return next
-                (new ErrorResponse(error.message,500))
+      if(!savedUser){
+        return next 
+          (new ErrorResponse('This email does not exist', 401))
         }
 
-        }catch(error) {
-            return next (error) 
+      // * Generate token
+      const resetToken = crypto.randomBytes(20).toString('hex');
+
+      // + Hash and set to resetPasswordToken
+      savedUser.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+  
+      // - Set token expire time
+      savedUser.resetPasswordExpire = Date.now() + 30 * 60 * 1000
+  
+      await savedUser.save({validateBeforeSave: false});
+  
+      // ? create password reset url
+      const url = `${req.protocol}://${req.get(
+        "host"
+      )}/v1/user/reset-password/${resetToken}`;
+  
+      try {
+        await sendEmail( 
+          email, 
+          url, 
+          "Reset your password"
+        )
+  
+        res.json({
+          status: 200,
+          success: true,
+          message: `Email sent to ${savedUser.email}`
+        })
+      } catch (error) {
+          savedUser.resetPasswordToken = undefined;
+          savedUser.resetPasswordExpire = undefined;
+  
+          await savedUser.save({validateBeforeSave: false})
+            return next
+              (new ErrorResponse(error.message,500))
         }
-    },
+
+    }catch(error) {
+      return next (error) 
+    }
+  },
    
-    resetPassword: async (req, res, next) => {
+  resetPassword: async (req, res, next) => {
 
-        const { token } = req.params;
-        const { password, confirmPassword } = req.body;
+    const { token } = req.params;
+    const { password, confirmPassword } = req.body;
 
-        try {
-              // Hash URL token
-            const resetPasswordToken = crypto.createHash('sha256').update(token).digest('hex')
+    try {
+      // Hash URL token
+      const resetPasswordToken = crypto.createHash('sha256').update(token).digest('hex')
 
-            const savedUser = await User.findOne({
-                resetPasswordToken,
-                resetPasswordExpire: { $gt: Date.now() }
-            })
+      const savedUser = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() }
+      })
 
-            if(!savedUser){
-                return next
-                    (new ErrorResponse('Password reset token is invalid or has expired. Please try again.', 400))
-                }
+      if(!savedUser){
+        return next
+          (new ErrorResponse('Password reset token is invalid or has expired. Please try again.', 400))
+      }
 
-            if (password !== confirmPassword) {
-                return next
-                    (new ErrorResponse('Passwords do not match.', 400))
-            }
+      if (password !== confirmPassword) {
+        return next
+          (new ErrorResponse('Passwords do not match.', 400))
+      }
 
-            // Setup new password
-            const salt = await bcrypt.genSalt(10);
-            const hashedPassword = await bcrypt.hash(password,salt);
+      // Setup new password
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password,salt);
 
-            savedUser.password = hashedPassword;
+      savedUser.password = hashedPassword;
 
-            savedUser.resetPasswordToken = undefined;
-            savedUser.resetPasswordExpire = undefined;
+      savedUser.resetPasswordToken = undefined;
+      savedUser.resetPasswordExpire = undefined;
 
-            await savedUser.save();
+      await savedUser.save();
 
-            const payload = {userid: savedUser._id}
-            const authToken = await jwt.sign(payload,process.env.SECRET,{expiresIn: '7d'})
+      const payload = {userid: savedUser._id}
+      const authToken = await jwt.sign(payload,process.env.SECRET,{expiresIn: '7d'})
 
-            GenerateToken(savedUser, 200,res,authToken)
-        } 
-         catch(error) {
-            return next (error) 
-        }
-    },
+      GenerateToken(savedUser, 200,res,authToken)
+    } catch(error) {
+      return next (error) 
+    }
+  },
 
-    updatePassword: async (req, res, next) => {
-        const { password, newPassword, confirmPassword } = req.body;
+  updatePassword: async (req, res, next) => {
+    const { password, newPassword, confirmPassword } = req.body;
 
-        try{
-            const savedUser = await User.findById(req.savedUser.id).select("+password");
+    try{
+      const savedUser = await User.findById(req.savedUser.id).select("+password");
 
-            const isMatch = await bcrypt.compare(password, savedUser.password);
+      const isMatch = await bcrypt.compare(password, savedUser.password);
 
-            if (!isMatch) {
-                return next
-                    (new ErrorResponse("Old password is incorrect", 400));
-            }
+      if (!isMatch) {
+        return next
+          (new ErrorResponse("Old password is incorrect", 400));
+      }
           
-            if (newPassword !== confirmPassword) {
-                return next
-                    (new ErrorResponse("password does not match", 400));
-            }
+      if (newPassword !== confirmPassword) {
+        return next
+          (new ErrorResponse("password does not match", 400));
+      }
 
-            const salt = bcrypt.genSaltSync(10);
-            const hashPassword = bcrypt.hashSync(newPassword, salt);
+      const salt = bcrypt.genSaltSync(10);
+      const hashPassword = bcrypt.hashSync(newPassword, salt);
           
-            savedUser.password = hashPassword;
+      savedUser.password = hashPassword;
           
-            await savedUser.save();
+      await savedUser.save();
           
-            GenerateToken(savedUser, 200, res);
+      GenerateToken(savedUser, 200, res);
 
-        } catch(error){
-            return next(error)
-        }
-    },
+    } catch(error){
+      return next(error)
+    }
+  },
 
-    updateUser: async (req, res, next) => {
+  updateUser: async (req, res, next) => {
 
-        const {firstname, lastname, email} = req.body;
+    const {firstname, lastname, email} = req.body;
 
-        try {
-            const newUser = {
-                firstname,
-                lastname,
-                email
-            }
+    try {
+      const newUser = {
+        firstname,
+        lastname,
+        email
+      }
             
-            if(req.body.avatar !== ''){
-              const savedUser = await User.findById(req.savedUser.id)
+      if(req.body.avatar !== ''){
+        const savedUser = await User.findById(req.savedUser.id)
               
-              const imageId = savedUser.avatar[0].public_id
+        const imageId = savedUser.avatar[0].public_id
               
-              await cloudinary.v2.uploader.destroy(imageId)
+        await cloudinary.v2.uploader.destroy(imageId)
               
-              const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
-                folder: 'avatar',
-                width: 150,
-                height:150,
-                crop: 'scale'
-              })
+        const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+          folder: 'avatar',
+          quality: 60,
+          width: 'auto',
+          crop: 'scale'
+        })
               
-              newUser.avatar = {
-                public_id: myCloud.public_id,
-                url: myCloud.secure_url
-              }
-            }
-
-            const savedUser = await User.findByIdAndUpdate(req.savedUser.id, newUser, {
-                new: true,
-                runValidators: true,
-                useFindAndModify: false,
-              });
-
-            res.json({
-                status: 200,
-                success: true,
-                message: "Profile updated successfully",
-                savedUser
-              })
-
-        }catch(err) {
-            return next (err) 
+        newUser.avatar = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url
         }
-    },
+      }
+
+      const savedUser = await User.findByIdAndUpdate(req.savedUser.id, newUser, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+      });
+
+      res.json({
+        status: 200,
+        success: true,
+        message: "Profile updated successfully",
+        savedUser
+      })
+
+    }catch(err) {
+      return next (err) 
+    }
+  },
 
 
-    UserInfo: async (req, res, next) => {
-        try {
-            const thisUser = await User.findById(req.savedUser.id)
+  UserInfo: async (req, res, next) => {
+    try {
+      const thisUser = await User.findById(req.savedUser.id)
 
-            res.json({
-                status: 200,
-                success: true,
-                thisUser
-            })
-        } 
-         catch(error) {
-            return next (error) 
-        }
-    },
+      res.json({
+        status: 200,
+        success: true,
+        thisUser
+      })
+    } 
+      catch(error) {
+        return next (error) 
+    }
+  },
 
-    SingleUser: async (req, res, next) => {
-        try{
-            const oneUser = await User.findById(req.params.id);
+  SingleUser: async (req, res, next) => {
+    try{
+      const oneUser = await User.findById(req.params.id);
 
-            if(!oneUser){
-                return next
-                    (new ErrorResponse(`User with id ${req.params.id} does not exist`, 400))
-            }
+      if(!oneUser){
+        return next
+          (new ErrorResponse(`User with id ${req.params.id} does not exist`, 400))
+      }
 
-            return res.json({
-                status: 200,
-                success: true,
-                oneUser
-            })
-        } catch(err){
-            return next (err)
-        }
-    },
+      return res.json({
+        status: 200,
+        success: true,
+        oneUser
+      })
+    } catch(err){
+      return next (err)
+    }
+  },
 
-    AllUserInfo: async (req, res, next) => {
-        try {
-            const allUser = await User.find()
-                                    .select('-password')
-                                    .select('-confirmPassword')
-                                    .exec()
-            res.json({
-                status: 200,
-                success: true,
-                allUser
-            })
-        }catch(err) {
-            return next (err) 
-        }
-    },
+  AllUserInfo: async (req, res, next) => {
+    try {
+      const allUser = await User.find()
+                                .select('-password')
+                                .select('-confirmPassword')
+                                .exec()
+      res.json({
+        status: 200,
+        success: true,
+        allUser
+      })
+    }catch(err) {
+      return next (err) 
+    }
+  },
 
     updateUserRole: async (req, res, next) => {
 
@@ -369,39 +368,39 @@ const UserController = {
         }
     },
 
-    logout: async (req, res, next) => {
-        try {
-            res.cookie("token", null, {
-                expires: new Date(Date.now()),
-                httpOnly: true,
-              });
+  logout: async (req, res, next) => {
+    try {
+      res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true,
+      });
 
-            res.json({
-                status: 200,
-                message: 'Logged out successfully'
-            })
-        }catch(err) {
-            return next (err) 
-        }
-    },
-
-    deleteUser: async (req, res, next) => {
-        try {
-            const oneProfile = await User.findByIdAndDelete(req.params.id);
-
-            if(!oneProfile){
-                return next
-                    (new ErrorResponse(`User with id ${req.params.id} does not exist`, 400))
-            }
-
-            res.json({
-                status: 200,
-                message: 'User deleted successfully'
-            })
-        }catch(err) {
-            return next (err) 
-        }
+      res.json({
+        status: 200,
+        message: 'Logged out successfully'
+      })
+    }catch(err) {
+      return next (err) 
     }
+  },
+
+  deleteUser: async (req, res, next) => {
+    try {
+      const oneProfile = await User.findByIdAndDelete(req.params.id);
+
+      if(!oneProfile){
+        return next
+          (new ErrorResponse(`User with id ${req.params.id} does not exist`, 400))
+      }
+
+      res.json({
+        status: 200,
+        message: 'User deleted successfully'
+      })
+    }catch(err) {
+      return next (err) 
+    }
+  }
 }
 
 export default UserController;
