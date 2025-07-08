f29563a7e0c549afad2226ff6e99877b570c6e6c
Stella Oseyomon <78904383+stellaose@users.noreply.github.com>
2025-07-08 18:20:32 +0100

    updating the database

--- server/Database/index.js
+++ server/Database/index.js
@@ -1,19 +1,19 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = `mongodb+srv://${process.env.DB_DATABASE}:${process.env.DB_PASSWORD}@${process.env.DB_USER}.ma5qp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const db = `mongodb+srv://${process.env.DB_DATABASE}:${process.env.DB_PASSWORD}@${process.env.DB_USER}.y9jncnc.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.DB_APP_NAME}`;
 
 const databaseConnection = {
-    getConnect: () => {
-        mongoose
-            .connect(db, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            })
-            .then(() => console.log('Database Connected Successfully'))
-    }
-}
+  getConnect: () => {
+    mongoose
+      .connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => console.log("Database Connected Successfully"));
+  },
+};
 
 export default databaseConnection;
